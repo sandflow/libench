59be2419c2a62ad4cf5dad4fd94a5bc1ece18af5
Pierre-Anthony Lemieux <pal@palemieux.com>
2022-02-04 21:11:53 -0800

    Added graph

--- src/main/python/make_page.py
+++ src/main/python/make_page.py
@@ -3,6 +3,7 @@ import os
 import argparse
 import json
 import typing
+import subprocess
 from dataclasses import dataclass, field
 import os.path
 import matplotlib.pyplot as plt
@@ -21,14 +22,79 @@ class Image:
   name: str
   codecs : typing.Iterable[Codec] = field(default_factory=list)
 
-
 def build(build_dir, images: typing.Iterable[Image]):
+
+  # render image results
+
+  results = {
+    "date": datetime.now().isoformat(),
+    "version": "unknown",
+    "images": []
+  }
+
+  for image in images:
+    rendered_image = {}
+
+    rendered_image["name"] = image.name
+
+    # decode plot
+    efficiencies = tuple(map(lambda x : x.coding_efficiency * 100, image.codecs))
+    decode_times = tuple(map(lambda y : y.decode_time * 1000, image.codecs))
+    decode_plot_fn = image.name + "_decode.svg"
+    _, ax = plt.subplots()
+    ax.scatter(efficiencies, decode_times)
+
+    for i, codec in enumerate(image.codecs):
+      ax.annotate(codec.name, (efficiencies[i], decode_times[i]))
+    plt.tick_params(which='both', bottom=False, top=False, left=False, right=False)
+    ax.grid(True)
+    ax.set(xlim=(0, 100))
+    ax.set_ylabel("Decoding speed (ms)")
+    ax.set_xlabel("Coding efficiency (%)")
+    ax.set_title("Decoding performance")
+    plt.show()
+    plt.savefig(os.path.join(build_dir, decode_plot_fn))
+
+    rendered_image["decode_plot_fn"] = decode_plot_fn
+
+    # encode plot
+    encode_times = tuple(map(lambda y : y.encode_time * 1000, image.codecs))
+    encode_plot_fn = image.name + "_encode.svg"
+    _, ax = plt.subplots()
+    ax.scatter(efficiencies, encode_times)
+
+    for i, codec in enumerate(image.codecs):
+      ax.annotate(codec.name, (efficiencies[i], encode_times[i]))
+    plt.tick_params(which='both', bottom=False, top=False, left=False, right=False)
+    ax.grid(True)
+    ax.set(xlim=(0, 100))
+    ax.set_ylabel("Encoding speed (ms)")
+    ax.set_xlabel("Coding efficiency (%)")
+    ax.set_title("Encoding performance")
+    plt.show()
+    plt.savefig(os.path.join(build_dir, encode_plot_fn))
+
+    rendered_image["encode_plot_fn"] = encode_plot_fn
+
+    rendered_image["codecs"] = []
+
+    for codec in image.codecs:
+      rendered_image["codecs"].append({
+        "name": codec.name,
+        "encode_time": codec.encode_time * 1000,
+        "decode_time": codec.decode_time * 1000,
+        "coding_efficiency" : round(codec.coding_efficiency * 100)
+    })
+
+    results["images"].append(rendered_image)
+
+  # apply template
 
   with open("src/main/resources/hbs/index.hbs", "r", encoding="utf-8") as template_file:
     with open(os.path.join(build_dir, "index.html"), "w", encoding="utf-8") as index_file:
-      index_file.write(chevron.render(template_file, {"date": datetime.now().isoformat(), "version": "unknown", "images": images}))
+      index_file.write(chevron.render(template_file, results))
 
-if __name__ == "main":
+def main():
   parser = argparse.ArgumentParser(description="Generate static web page with lossless coding results.")
   parser.add_argument("manifest_path", type=str, help="Path of the manifest file")
   args = parser.parse_args()
@@ -46,3 +112,6 @@ if __name__ == "main":
   ax.scatter([1,2], [3,4])
   plt.show()
   plt.savefig('build/a.png')
+
+if __name__ == "main":
+  main()
--- src/test/python/test_make_page.py
+++ src/test/python/test_make_page.py
@@ -11,10 +11,11 @@ class MakePageTest(unittest.TestCase):
     os.makedirs(MakePageTest.BUILD_DIR, exist_ok=True)
 
   def test_make(self):
-    image = make_page.Image(name="rgba")
+    image = make_page.Image(name="Sample PNG RGBA image")
     image.codecs.extend((
-      make_page.Codec(name="HT", coding_efficiency="0.5", encode_time="0.5", decode_time="0.1"),
-      make_page.Codec(name="J2K1", coding_efficiency="0.5", encode_time="0.5", decode_time="0.1")
+      make_page.Codec(name="HT", coding_efficiency=0.5, encode_time=0.5, decode_time=0.5),
+      make_page.Codec(name="J2K1", coding_efficiency=0.55, encode_time=0.1, decode_time=0.1),
+      make_page.Codec(name="JPG", coding_efficiency=0.65, encode_time=0.3, decode_time=0.3)
     ))
 
     make_page.build(MakePageTest.BUILD_DIR, [image])
