dba0656e2ac495ccc36ebb5a9c51ea5152b7ac20
Pierre-Anthony Lemieux <pal@palemieux.com>
2022-05-26 17:18:28 -0700

    Clean-up OpenHTJ2K support

--- src/main/python/make_page.py
+++ src/main/python/make_page.py
@@ -40,7 +40,7 @@ CODEC_PREFS = {
     "j2k_ht_ojph": CodecPreferences(color="#41b6e6", marker="o"),
     "j2k_1_kdu": CodecPreferences(color="#41b6e6", marker="v"),
     "j2k_ht_kdu": CodecPreferences(color="#41b6e6", marker="s"),
-    "j2k_ht_ohtj2k": CodecPreferences(color="#94a596", marker="p"),
+    "j2k_ht_ohtj2k": CodecPreferences(color="#41b6e6", marker="p"),
     "jxl": CodecPreferences(color="#e56db1", marker="o"),
     "qoi": CodecPreferences(color="#dc582a", marker="o"),
     "png": CodecPreferences(color="#f2c75c", marker="o"),
