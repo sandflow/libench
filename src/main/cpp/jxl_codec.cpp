ff5ec6959ee27b24a5185cc78c972ccc52f07a16
Luca Versari <veluca93@gmail.com>
2024-03-03 16:09:35 +0100

    Bump libjxl to v0.10, force single threaded decoding.

--- src/main/cpp/jxl_codec.cpp
+++ src/main/cpp/jxl_codec.cpp
@@ -1,47 +1,116 @@
 #include "jxl_codec.h"
-#include <inttypes.h>
+#include "jxl/decode_cxx.h"
+#include "jxl/encode_cxx.h"
+#include "jxl/types.h"
 #include <climits>
+#include <cstdlib>
+#include <inttypes.h>
+#include <jxl/encode.h>
 #include <memory>
 #include <stdexcept>
 #include <vector>
-#include "fast_lossless.h"
-#include "jxl/decode_cxx.h"
-#include "jxl/encode_cxx.h"
-#include "jxl/resizable_parallel_runner_cxx.h"
-#include "jxl/thread_parallel_runner_cxx.h"
 
 /*
  * JXLEncoder
  */
 
-template class libench::JXLEncoder<0>;
+template class libench::JXLEncoder<1>;
 template class libench::JXLEncoder<2>;
+template class libench::JXLEncoder<3>;
 
-template <int E>
-libench::JXLEncoder<E>::JXLEncoder(){};
+template <int E> libench::JXLEncoder<E>::JXLEncoder(){};
 
-template <int E>
-libench::JXLEncoder<E>::~JXLEncoder() {
+template <int E> libench::JXLEncoder<E>::~JXLEncoder() {
   free(this->cb_.codestream);
 };
 
+template <int E, bool rgba>
+static size_t JxlEncode(void *image_data, size_t width, size_t height,
+                        uint8_t **encoded) {
+  auto enc = JxlEncoderMake(/*memory_manager=*/nullptr);
+
+  JxlPixelFormat pixel_format = {rgba ? 4 : 3, JXL_TYPE_UINT8,
+                                 JXL_NATIVE_ENDIAN, 0};
+
+  JxlBasicInfo basic_info;
+  JxlEncoderInitBasicInfo(&basic_info);
+  basic_info.xsize = width;
+  basic_info.ysize = height;
+  basic_info.bits_per_sample = 8;
+  basic_info.exponent_bits_per_sample = 0;
+  basic_info.uses_original_profile = JXL_TRUE;
+  if (rgba) {
+    basic_info.alpha_bits = 8;
+    basic_info.num_extra_channels = 1;
+  }
+  if (JXL_ENC_SUCCESS != JxlEncoderSetBasicInfo(enc.get(), &basic_info)) {
+    throw std::runtime_error("JxlEncoderSetBasicInfo failed\n");
+  }
+
+  JxlColorEncoding color_encoding = {};
+  JxlColorEncodingSetToSRGB(&color_encoding, false);
+  if (JXL_ENC_SUCCESS !=
+      JxlEncoderSetColorEncoding(enc.get(), &color_encoding)) {
+    throw std::runtime_error("JxlEncoderSetColorEncoding failed\n");
+  }
+
+  JxlEncoderFrameSettings *frame_settings =
+      JxlEncoderFrameSettingsCreate(enc.get(), nullptr);
+
+  if (JXL_ENC_SUCCESS != JxlEncoderSetFrameLossless(frame_settings, JXL_TRUE)) {
+    throw std::runtime_error("JxlEncoderSetFrameLossless failed\n");
+  }
+
+  if (JXL_ENC_SUCCESS != JxlEncoderFrameSettingsSetOption(
+                             frame_settings, JXL_ENC_FRAME_SETTING_EFFORT, E)) {
+    throw std::runtime_error("JxlEncoderFrameSettingsSetOption failed\n");
+  }
+
+  if (JXL_ENC_SUCCESS !=
+      JxlEncoderAddImageFrame(frame_settings, &pixel_format,
+                              static_cast<const void *>(image_data),
+                              pixel_format.num_channels * width * height)) {
+    throw std::runtime_error("JxlEncoderAddImageFrame failed\n");
+  }
+  JxlEncoderCloseInput(enc.get());
+
+  size_t size = pixel_format.num_channels * width * height;
+  *encoded = (uint8_t *)malloc(size);
+  uint8_t *next_out = *encoded;
+  size_t avail_out = size - (next_out - *encoded);
+  JxlEncoderStatus process_result = JXL_ENC_NEED_MORE_OUTPUT;
+  while (process_result == JXL_ENC_NEED_MORE_OUTPUT) {
+    process_result = JxlEncoderProcessOutput(enc.get(), &next_out, &avail_out);
+    if (process_result == JXL_ENC_NEED_MORE_OUTPUT) {
+      size_t offset = next_out - *encoded;
+      size *= 2;
+      *encoded = (uint8_t *)realloc(*encoded, size);
+      next_out = *encoded + offset;
+      avail_out = size - offset;
+    }
+  }
+  if (JXL_ENC_SUCCESS != process_result) {
+    throw std::runtime_error("JxlEncoderProcessOutput failed\n");
+  }
+
+  return next_out - *encoded;
+}
+
 template <int E>
-libench::CodestreamContext libench::JXLEncoder<E>::encodeRGB8(const ImageContext &image) {
+libench::CodestreamContext
+libench::JXLEncoder<E>::encodeRGB8(const ImageContext &image) {
   free(this->cb_.codestream);
-
-  this->cb_.size = FastLosslessEncode(image.planes8[0], image.width, image.width * 3, image.height, 3, 8, E,
-                                      &this->cb_.codestream);
-
+  cb_.size = JxlEncode<E, false>(image.planes8[0], image.width, image.height,
+                                 &cb_.codestream);
   return this->cb_;
 }
 
 template <int E>
-libench::CodestreamContext libench::JXLEncoder<E>::encodeRGBA8(const ImageContext &image) {
+libench::CodestreamContext
+libench::JXLEncoder<E>::encodeRGBA8(const ImageContext &image) {
   free(this->cb_.codestream);
-
-  this->cb_.size = FastLosslessEncode(image.planes8[0], image.width, image.width * 4, image.height, 4, 8, E,
-                                      &this->cb_.codestream);
-
+  cb_.size = JxlEncode<E, true>(image.planes8[0], image.width, image.height,
+                                &cb_.codestream);
   return this->cb_;
 }
 
@@ -51,21 +120,22 @@ libench::CodestreamContext libench::JXLEncoder<E>::encodeRGBA8(const ImageContex
 
 libench::JXLDecoder::JXLDecoder(){};
 
-libench::ImageContext libench::JXLDecoder::decodeRGB8(const CodestreamContext& cs) {
+libench::ImageContext
+libench::JXLDecoder::decodeRGB8(const CodestreamContext &cs) {
   return this->decode8(cs, 3);
 }
 
-libench::ImageContext libench::JXLDecoder::decodeRGBA8(const CodestreamContext& cs) {
+libench::ImageContext
+libench::JXLDecoder::decodeRGBA8(const CodestreamContext &cs) {
   return this->decode8(cs, 4);
 }
 
-libench::ImageContext libench::JXLDecoder::decode8(const CodestreamContext& cs, uint8_t num_comps) {
+libench::ImageContext libench::JXLDecoder::decode8(const CodestreamContext &cs,
+                                                   uint8_t num_comps) {
   libench::ImageContext image;
 
-  image.format = num_comps == 3 ? libench::ImageFormat::RGB8 : libench::ImageFormat::RGBA8;
-
-  // Multi-threaded parallel runner.
-  auto runner = JxlResizableParallelRunnerMake(nullptr);
+  image.format =
+      num_comps == 3 ? libench::ImageFormat::RGB8 : libench::ImageFormat::RGBA8;
 
   auto dec = JxlDecoderMake(nullptr);
   if (JXL_DEC_SUCCESS !=
@@ -73,12 +143,6 @@ libench::ImageContext libench::JXLDecoder::decode8(const CodestreamContext& cs, 
                                                JXL_DEC_COLOR_ENCODING |
                                                JXL_DEC_FULL_IMAGE)) {
     throw std::runtime_error("JxlDecoderSubscribeEvents failed\n");
-  }
-
-  if (JXL_DEC_SUCCESS != JxlDecoderSetParallelRunner(dec.get(),
-                                                     JxlResizableParallelRunner,
-                                                     runner.get())) {
-    throw std::runtime_error("JxlDecoderSetParallelRunner failed\n");
   }
 
   JxlBasicInfo info;
@@ -100,21 +164,17 @@ libench::ImageContext libench::JXLDecoder::decode8(const CodestreamContext& cs, 
       }
       image.width = info.xsize;
       image.height = info.ysize;
-      JxlResizableParallelRunnerSetThreads(
-          runner.get(),
-          JxlResizableParallelRunnerSuggestThreads(info.xsize, info.ysize));
     } else if (status == JXL_DEC_COLOR_ENCODING) {
       // Get the ICC color profile of the pixel data (but ignore it)
       size_t icc_size;
       if (JXL_DEC_SUCCESS !=
-          JxlDecoderGetICCProfileSize(
-              dec.get(), &format, JXL_COLOR_PROFILE_TARGET_DATA, &icc_size)) {
+          JxlDecoderGetICCProfileSize(dec.get(), JXL_COLOR_PROFILE_TARGET_DATA,
+                                      &icc_size)) {
         throw std::runtime_error("JxlDecoderGetICCProfileSize failed\n");
       }
       std::vector<uint8_t> icc_profile(icc_size);
       if (JXL_DEC_SUCCESS != JxlDecoderGetColorAsICCProfile(
-                                 dec.get(), &format,
-                                 JXL_COLOR_PROFILE_TARGET_DATA,
+                                 dec.get(), JXL_COLOR_PROFILE_TARGET_DATA,
                                  icc_profile.data(), icc_profile.size())) {
         throw std::runtime_error("JxlDecoderGetColorAsICCProfile failed\n");
       }
@@ -128,7 +188,7 @@ libench::ImageContext libench::JXLDecoder::decode8(const CodestreamContext& cs, 
         throw std::runtime_error("Invalid out buffer size");
       }
       this->pixels_.resize(image.height * image.width * num_comps);
-      void* pixels_buffer = (void*)this->pixels_.data();
+      void *pixels_buffer = (void *)this->pixels_.data();
       size_t pixels_buffer_size = this->pixels_.size() * sizeof(float);
       if (JXL_DEC_SUCCESS != JxlDecoderSetImageOutBuffer(dec.get(), &format,
                                                          pixels_buffer,
--- src/main/cpp/main.cpp
+++ src/main/cpp/main.cpp
@@ -1,17 +1,17 @@
-#include <stdio.h>
-#include <time.h>
+#include "cxxopts.hpp"
+#include "ffv1_codec.h"
+#include "jxl_codec.h"
+#include "kduht_codec.h"
+#include "ojph_codec.h"
+#include "png_codec.h"
+#include "qoi_codec.h"
+#include <chrono>
 #include <fstream>
 #include <iomanip>
 #include <iostream>
 #include <sstream>
-#include <chrono>
-#include "cxxopts.hpp"
-#include "jxl_codec.h"
-#include "ojph_codec.h"
-#include "qoi_codec.h"
-#include "kduht_codec.h"
-#include "png_codec.h"
-#include "ffv1_codec.h"
+#include <stdio.h>
+#include <time.h>
 
 #define STB_IMAGE_IMPLEMENTATION
 #define STBI_ONLY_PNG
@@ -28,11 +28,11 @@ struct TestContext {
   std::vector<std::chrono::system_clock::time_point::duration> decode_times;
 };
 
-std::ostream& operator<<(std::ostream& os, const TestContext& ctx) {
+std::ostream &operator<<(std::ostream &os, const TestContext &ctx) {
   os << "{" << std::endl;
 
   os << "\"decodeTimes\" : [";
-  for (const auto& t : ctx.decode_times) {
+  for (const auto &t : ctx.decode_times) {
     os << std::chrono::duration<double>(t).count();
     if (&t != &ctx.decode_times.back()) {
       os << ", ";
@@ -41,7 +41,7 @@ std::ostream& operator<<(std::ostream& os, const TestContext& ctx) {
   os << "]," << std::endl;
 
   os << "\"encodeTimes\" : [";
-  for (const auto& t : ctx.encode_times) {
+  for (const auto &t : ctx.encode_times) {
     os << std::chrono::duration<double>(t).count();
     if (&t != &ctx.encode_times.back()) {
       os << ", ";
@@ -51,18 +51,18 @@ std::ostream& operator<<(std::ostream& os, const TestContext& ctx) {
 
   os << "\"imageSize\" : " << ctx.image_sz << "," << std::endl;
 
-  os << "\"codestreamSize\" : " << ctx.codestream_sz << ","  << std::endl;
+  os << "\"codestreamSize\" : " << ctx.codestream_sz << "," << std::endl;
 
-  os << "\"imageWidth\" : " << ctx.image.width  << "," << std::endl;
+  os << "\"imageWidth\" : " << ctx.image.width << "," << std::endl;
 
-   os << "\"imageHeight\" : " << ctx.image.height  << std::endl;
+  os << "\"imageHeight\" : " << ctx.image.height << std::endl;
 
   os << "}" << std::endl;
 
   return os;
 }
 
-libench::ImageContext load_image(const std::string& filepath) {
+libench::ImageContext load_image(const std::string &filepath) {
   libench::ImageContext image;
 
   size_t start = filepath.find_last_of(".");
@@ -73,9 +73,10 @@ libench::ImageContext load_image(const std::string& filepath) {
     int height;
     int width;
     int num_comps;
-    
-    image.planes8[0] = stbi_load(filepath.c_str(), &width, &height, &num_comps, 0);
-    if (! image.planes8[0]) {
+
+    image.planes8[0] =
+        stbi_load(filepath.c_str(), &width, &height, &num_comps, 0);
+    if (!image.planes8[0]) {
       throw std::runtime_error("Cannot read image file");
     }
 
@@ -83,14 +84,14 @@ libench::ImageContext load_image(const std::string& filepath) {
     image.width = width;
 
     switch (num_comps) {
-      case 3:
-        image.format = libench::ImageFormat::RGB8;
-        break;
-      case 4:
-        image.format = libench::ImageFormat::RGBA8;
-        break;
-      default:
-        throw std::runtime_error("Only RGB or RGBA images are supported");
+    case 3:
+      image.format = libench::ImageFormat::RGB8;
+      break;
+    case 4:
+      image.format = libench::ImageFormat::RGBA8;
+      break;
+    default:
+      throw std::runtime_error("Only RGB or RGBA images are supported");
     }
 
   } else if (file_ext == "yuv") {
@@ -113,14 +114,14 @@ libench::ImageContext load_image(const std::string& filepath) {
 
       std::ifstream in(filepath);
 
-      for(uint8_t i = 0; i < image.format.num_planes(); i++) {
+      for (uint8_t i = 0; i < image.format.num_planes(); i++) {
 
-        image.planes16[i] = (uint16_t*) malloc(image.plane_size(i));
-        if (! image.planes16[i]) {
+        image.planes16[i] = (uint16_t *)malloc(image.plane_size(i));
+        if (!image.planes16[i]) {
           throw std::runtime_error("Cannot allocate memory");
         }
-        
-        in.read((char*) image.planes16[i], image.plane_size(i));
+
+        in.read((char *)image.planes16[i], image.plane_size(i));
         if (in.bad()) {
           throw std::runtime_error("Read failed");
         }
@@ -130,7 +131,6 @@ libench::ImageContext load_image(const std::string& filepath) {
       throw std::runtime_error("Unknown pixel format: " + pix_fmt);
     }
 
-    
   } else {
     throw std::runtime_error("Image file must be YUV or PNG");
   }
@@ -138,14 +138,14 @@ libench::ImageContext load_image(const std::string& filepath) {
   return image;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   cxxopts::Options options("libench", "Lossless image codec benchmark");
 
   options.add_options()("dir", "Codesteeam directory path",
                         cxxopts::value<std::string>())(
       "r,repetitions", "Codesteeam directory path",
-      cxxopts::value<int>()->default_value("5"))(
-      "file", "Input image", cxxopts::value<std::string>())(
+      cxxopts::value<int>()->default_value("5"))("file", "Input image",
+                                                 cxxopts::value<std::string>())(
       "codec", "Coded to profile", cxxopts::value<std::string>());
 
   options.parse_positional({"codec", "file"});
@@ -161,11 +161,14 @@ int main(int argc, char* argv[]) {
   } else if (result["codec"].as<std::string>() == "qoi") {
     encoder.reset(new libench::QOIEncoder());
     decoder.reset(new libench::QOIDecoder());
-  } else if (result["codec"].as<std::string>() == "jxl") {
+  } else if (result["codec"].as<std::string>() == "jxl_e3") {
+    encoder.reset(new libench::JXLEncoder<3>());
+    decoder.reset(new libench::JXLDecoder());
+  } else if (result["codec"].as<std::string>() == "jxl_e2") {
     encoder.reset(new libench::JXLEncoder<2>());
     decoder.reset(new libench::JXLDecoder());
-  } else if (result["codec"].as<std::string>() == "jxl_0") {
-    encoder.reset(new libench::JXLEncoder<0>());
+  } else if (result["codec"].as<std::string>() == "jxl") {
+    encoder.reset(new libench::JXLEncoder<1>());
     decoder.reset(new libench::JXLDecoder());
   } else if (result["codec"].as<std::string>() == "j2k_ht_kdu") {
     encoder.reset(new libench::KDUEncoder(true));
@@ -183,8 +186,7 @@ int main(int argc, char* argv[]) {
     throw std::runtime_error("Unknown encoder");
   }
 
-  
-  auto& filepath = result["file"].as<std::string>();
+  auto &filepath = result["file"].as<std::string>();
 
   libench::ImageContext in_img = load_image(filepath);
 
@@ -197,8 +199,8 @@ int main(int argc, char* argv[]) {
   test.decode_times.resize(repetitions);
   test.image_sz = in_img.total_bits() / 8;
 
-  /*std::ofstream in_raw(filepath + "." + result["codec"].as<std::string>() + ".in.raw");
-  in_raw.write((const char*) data, width * height * num_comps);
+  /*std::ofstream in_raw(filepath + "." + result["codec"].as<std::string>() +
+  ".in.raw"); in_raw.write((const char*) data, width * height * num_comps);
   in_raw.close();*/
 
   /* source hash */
@@ -213,13 +215,13 @@ int main(int argc, char* argv[]) {
     auto start = std::chrono::high_resolution_clock::now();
 
     if (in_img.format == libench::ImageFormat::RGB8) {
-        cs = encoder->encodeRGB8(in_img);
+      cs = encoder->encodeRGB8(in_img);
     } else if (in_img.format == libench::ImageFormat::RGBA8) {
-        cs = encoder->encodeRGBA8(in_img);
+      cs = encoder->encodeRGBA8(in_img);
     } else if (in_img.format == libench::ImageFormat::YUV422P10) {
-        cs = encoder->encodeYUV(in_img);
+      cs = encoder->encodeYUV(in_img);
     } else {
-        throw std::runtime_error("Unsupported number of components");
+      throw std::runtime_error("Unsupported number of components");
     }
 
     test.encode_times[i] = std::chrono::high_resolution_clock::now() - start;
@@ -243,7 +245,7 @@ int main(int argc, char* argv[]) {
         /* write the codestream */
 
         std::ofstream f(test.codestream_path);
-        f.write(reinterpret_cast<char*>(cs.codestream), cs.size);
+        f.write(reinterpret_cast<char *>(cs.codestream), cs.size);
         f.close();
       }
     }
@@ -266,9 +268,9 @@ int main(int argc, char* argv[]) {
 
     test.decode_times[i] = std::chrono::high_resolution_clock::now() - start;
 
-    /*std::ofstream out_raw(filepath + "." + result["codec"].as<std::string>() + ".out.raw");
-    out_raw.write((const char*) out_img.pixels, width * height * num_comps);
-    out_raw.close();*/
+    /*std::ofstream out_raw(filepath + "." + result["codec"].as<std::string>() +
+    ".out.raw"); out_raw.write((const char*) out_img.pixels, width * height *
+    num_comps); out_raw.close();*/
 
     /* bit exact compare */
 
@@ -278,13 +280,11 @@ int main(int argc, char* argv[]) {
 
     if (memcmp(decoded_hash, test.image_hash, MD5_BLOCK_SIZE))
       throw std::runtime_error("Image does not match");
-
   }
 
   std::cout << test;
 
-  for(uint8_t i = 0; i < in_img.format.num_planes(); i++) {
+  for (uint8_t i = 0; i < in_img.format.num_planes(); i++) {
     free(in_img.planes8[i]);
   }
-
 }
